59805bfd8df1a9b652bd131330b10b43932398c2
AtifxUMTS <atifalishahnaqvi3345@gmail.com>
2023-09-23 10:57:38 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = "kamran";
+let message = "How are doing today ${personName},hope you are doing well in these days.";
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,3 @@
+let personName: string = "kamran"
+let message: string = "How are doing today ${personName},hope you are doing well in these days."
+console.log(message)
